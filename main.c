442433ad5a9bb5f1c286af98ae7ca36d9aaa86a2
Yury Golub <yuragolub21@gmail.com>
2026-01-30 18:01:11 +0300

    Fixes.

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stddef.h>
+#include <stdbool.h>
 
 #ifdef _WIN32
 #include <windows.h>
@@ -10,16 +11,17 @@
 
 #ifdef _WIN32
 typedef void (*greet_t)(void);
+static const char *plugin_file_name = "plugin.dll";
 #else
-greet_t greet = NULL;
+static const char *plugin_file_name = "plugin.so";
 #endif // _WIN32
 
-int main(void)
+static void *plugin = NULL;
+static greet_t greet = NULL;
+
+bool reload_plugin(void)
 {
 #ifdef _WIN32
-    const char *plugin_file_name = "plugin.dll";
-    void *plugin = NULL;
-
     if (plugin != NULL)
     {
         FreeLibrary(plugin);
@@ -30,35 +32,46 @@ int main(void)
     if (plugin == NULL)
     {
         fprintf(stderr, "ERROR: could not load '%s'\r\n", plugin_file_name);
-        return 1;
+        return false;
     }
 
     const char *greet_func_name = "greet";
     printf("INFO: loading symbol '%s'\r\n", greet_func_name);
-    greet_t greet = (greet_t)GetProcAddress(plugin, greet_func_name);
+    greet = (greet_t)GetProcAddress(plugin, greet_func_name);
     if (greet == NULL)
     {
         fprintf(stderr, "ERROR: could not find '%s' symbol in '%s'\r\n", greet_func_name, plugin_file_name);
-        return 1;
+        return false;
     }
 #else
-    const char *plugin_file_name = "plugin.so";
-    void *handle = dlopen(plugin_file_name, RTLD_NOW);
-    if (handle == NULL)
+    plugin = dlopen(plugin_file_name, RTLD_NOW);
+    if (plugin == NULL)
     {
         fprintf(stderr, "ERROR: could not load %s: %s\n", plugin_file_name, dlerror());
-        return 1;
+        return false;
     }
 
     const char *greet_func_name = "greet";
-    greet_t greet = (greet_t)dlsym(handle, greet_func_name);
+    greet = (greet_t)dlsym(plugin, greet_func_name);
     if (greet == NULL)
     {
         fprintf(stderr, "ERROR: could not find %s symbol in %s\n", greet_func_name, plugin_file_name);
-        return 1;
+        return false;
     }
 #endif // _WIN32
 
+    return true;
+}
+
+int main(void)
+{
+    if (!reload_plugin()) return 1;
+
+    greet();
+    char input[10];
+    scanf_s("%s", input);
+
+    if (!reload_plugin()) return 1;
     greet();
 
     return 0;
